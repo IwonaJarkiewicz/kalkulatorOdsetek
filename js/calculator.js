2c58e7ad5d7c0d6539764eb4a5909ef386d3523e
IwonaJarkiewicz <politea@gmail.com>
2017-08-26 12:02:09 +0200

    liczy

--- js/calculator.js
+++ js/calculator.js
@@ -1,31 +1,28 @@
 'use strict';
-function(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek)
-{
- if(podatek == true) {
-     var oprocentowanieOpodatkowane = oprocentowanie
- }   else {
-      var oprocentowanieOpodatkowane = oprocentowanie;   
- }
+
+function obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek) {
+    if (podatek == true) {
+        var oprocentowanieOpodatkowane = oprocentowanie * 0.81;
+    } else {
+        var oprocentowanieOpodatkowane = oprocentowanie;
+    }
     var kapital = wplata * Math.pow(1 + (oprocentowanieOpodatkowane / okresKapitalizacji), iloscLat * okresKapitalizacji);
-    
+
     return parseFloat(kapital).toFixed(2);
-    
+
 }
 
 function oblicz() {
-    var wplata = perseInt(document.getElementById('wplata').value);
-    
+    var wplata = parseInt(document.getElementById('wplata').value);
+
     var iloscLat = parseInt(document.getElementById('ilosc-lat').value);
-    
+
     var okresKapitalizacji = parseInt(document.getElementById('okres-kapitalizacja').value);
-    
-    var oprocentowanie = parseInt(document.getElementById('oprocentowanie').value)/100;
-    
+
+    var oprocentowanie = parseInt(document.getElementById('oprocentowanie').value) / 100;
+
     var podatek = document.getElementById('podatek').checked;
-    
+
     var wynik = obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek);
     document.getElementById('wynik').innerHTML = wynik;
 }
-
-
-
